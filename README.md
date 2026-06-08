dd0674e68df4b1ef70adebbc7e84191d893793f2
ImDarkTom <darktomg21@gmail.com>
2026-06-08 22:32:02 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# GNOME Extension - Slider Percentages
+
+tba
+
+## Screenshots
+
+> tba
+
+## Extension Compatibility
+
+The following extensions have been tested and are compatible:
+
+<!-- - [Quick Settings Audio Panel](https://extensions.gnome.org/extension/5940/quick-settings-audio-panel/) (v103 tested) -->
+
+## Development
+
+To install the extension from source:
+
+```sh
+git clone https://github.com/ImDarkTom/slider-percentages-gnome
+cd slider-percentages-gnome/
+make install
+```
+
+The extension will be available after you log out and log back in, or, you can start a test shell to see changes by running:
+
+```sh
+make test-shell
+```
+
+## Version Support
+
+The following GNOME versions have been tested and are supported:
+
+* 46
+* 47
+* 48
+* 49
+* 50
+
+## License
+
+[AGPL-3.0](https://github.com/user-attachments/assets/07590f28-837d-4520-b807-820dbc4ca81a)
---
+++ extension.js
@@ -0,0 +1,74 @@
+import St from 'gi://St';
+import Clutter from 'gi://Clutter';
+import GLib from 'gi://GLib';
+import Gvc from 'gi://Gvc';
+
+import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+
+export default class SliderPercentagesExtension extends Extension {
+    enable() {
+        // Connect to mixer to track sink changes
+        this._mixer = new Gvc.MixerControl({ name: this.uuid });
+        this._mixer.connectObject('default-sink-changed', (mixer) => this._onSinkChanged(mixer), this);
+        this._mixer.open();
+
+        this._idleId = GLib.idle_add(GLib.PRIORITY_DEFAULT_IDLE, () => {
+            this._label = new St.Label({
+                text: '--%',
+                y_align: Clutter.ActorAlign.CENTER,
+                style: 'min-width: 3em; text-align: right;',
+            });
+
+            const quickSettingsMenu = Main.panel.statusArea.quickSettings.menu;
+
+            const sliderRow = quickSettingsMenu._grid.get_children()[1].get_first_child();
+            // [mute button] [slider] [<our inserted label>] [settings button]
+            sliderRow.insert_child_at_index(this._label, 2);
+
+            this._update();
+
+            this._idleId = 0;
+            return GLib.SOURCE_REMOVE;
+        });
+    }
+
+    _onSinkChanged(mixer) {
+        this._sink?.disconnectObject(this);
+
+        this._sink = mixer.get_default_sink();
+        if (!this._sink) return;
+
+        this._sink.connectObject(
+            'notify::volume', () => this._update(),
+            'notify::is-muted', () => this._update(),
+            this
+        );
+
+        this._update();
+    }
+
+    _update() {
+        if (!this._label || !this._sink) return;
+
+        const volumePercent = Math.round(this._sink.get_volume() / this._mixer.get_vol_max_norm() * 100);
+        this._label.text = this._sink.get_is_muted() ? '0%' : `${volumePercent}%`;
+    }
+
+    disable() {
+        if (this._idleId) {
+            GLib.source_remove(this._idleId);
+            this._idleId = 0;
+        }
+        
+        this._sink?.disconnectObject(this);
+        this._sink = null;
+
+        this._mixer?.disconnectObject(this);
+        this._mixer?.close();
+        this._mixer = null;
+
+        this._label?.destroy();
+        this._label = null;
+    }
+}

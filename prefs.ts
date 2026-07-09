f510ff25e699aceb9e676833ad4e1091af69ec9d
ImDarkTom <darktomg21@gmail.com>
2026-07-09 23:31:36 +0300

    refactor: utility method for switch rows

--- prefs.ts
+++ prefs.ts
@@ -4,14 +4,13 @@ import { ExtensionPreferences, gettext as _ } from 'resource:///org/gnome/Shell/
 import Gtk from 'gi://Gtk';
 
 export default class SliderPercentagesPreferences extends ExtensionPreferences {
-    static createLabelPrefsGroup(settings: Gio.Settings): Adw.PreferencesGroup {
+    private static createLabelPrefsGroup(settings: Gio.Settings): Adw.PreferencesGroup {
         const labelGroup = new Adw.PreferencesGroup({
             title: _('Label'),
             description: _('Customise the added label'),
         });
 
 
-        // Font weight
         const fontWeightRow = new Adw.ComboRow({
             title: _('Font Weight'),
             subtitle: _('How heavy the font is'),
@@ -30,17 +29,15 @@ export default class SliderPercentagesPreferences extends ExtensionPreferences {
         });
 
         labelGroup.add(fontWeightRow);
-
         settings.bind('label-font-weight', fontWeightRow, 'selected', Gio.SettingsBindFlags.DEFAULT);
 
 
-        // Font Family
         const fontFamilyRow = new Adw.ComboRow({
             title: _('Font Family'),
             subtitle: _('Which font family the labels use'),
             model: Gtk.StringList.new([
                 _('System Default'),
-                _('Sans serif'),
+                _('Sans Serif'),
                 _('Serif'),
                 _('Monospace'),
                 _('Custom'),
@@ -48,21 +45,17 @@ export default class SliderPercentagesPreferences extends ExtensionPreferences {
         });
 
         labelGroup.add(fontFamilyRow);
-
         settings.bind('label-font-family', fontFamilyRow, 'selected', Gio.SettingsBindFlags.DEFAULT);
 
 
-        // Custom font family
         const customFontFamilyRow = new Adw.EntryRow({
             title: _('Custom Font Family'),
             text: settings.get_string('label-custom-font-family'),
         });
-
         customFontFamilyRow.add_css_class('property');
         customFontFamilyRow.set_tooltip_text(_('Used when Font Family is set to Custom'));
 
         labelGroup.add(customFontFamilyRow);
-
         settings.bind(
             'label-custom-font-family',
             customFontFamilyRow,
@@ -70,18 +63,32 @@ export default class SliderPercentagesPreferences extends ExtensionPreferences {
             Gio.SettingsBindFlags.DEFAULT
         );
 
-        // Include percentage symbol
-        const labelPercentageSymbol = new Adw.SwitchRow({
-            title: _('Include Percentage Symbol'),
-            subtitle: _('Add the percentage symbol next to the label.'),
+
+        SliderPercentagesPreferences.createSwitchRow({
+            title: 'Include Percentage Symbol',
+            subtitle: 'Add the percentage symbol next to the label',
+            group: labelGroup,
+            settings,
+            key: 'label-include-percentage-symbol',
         });
 
-        labelGroup.add(labelPercentageSymbol);
-
-        settings.bind('label-include-percentage-symbol', labelPercentageSymbol, 'active', Gio.SettingsBindFlags.DEFAULT);
-
-
         return labelGroup;
+    }
+
+    private static createSwitchRow({ title, subtitle, group, settings, key }: {
+        title: string,
+        subtitle: string,
+        group: Adw.PreferencesGroup,
+        settings: Gio.Settings,
+        key: string,
+    }) {
+        const switchRow = new Adw.SwitchRow({
+            title: _(title),
+            subtitle: _(subtitle),
+        });
+
+        group.add(switchRow);
+        settings.bind(key, switchRow, 'active', Gio.SettingsBindFlags.DEFAULT);
     }
 
     fillPreferencesWindow(window: Adw.PreferencesWindow): Promise<void> {
@@ -101,19 +108,21 @@ export default class SliderPercentagesPreferences extends ExtensionPreferences {
         page.add(quickSettingsGroup);
 
         // QS items
-        const quickSettingsVolumeEnabled = new Adw.SwitchRow({
-            title: _('Volume Slider'),
-            subtitle: _('Show a volume percentage label next to the volume slider in the quick settings'),
+        SliderPercentagesPreferences.createSwitchRow({
+            title: 'Volume Slider',
+            subtitle: 'Show a volume percentage label next to the volume slider in the quick settings',
+            group: quickSettingsGroup,
+            settings,
+            key: 'quick-settings-volume'
         });
 
-        quickSettingsGroup.add(quickSettingsVolumeEnabled);
-
-        const quickSettingsBrightnessEnabled = new Adw.SwitchRow({
-            title: _('Brightness Slider'),
-            subtitle: _('Show a brightness percentage label next to the brightness slider in the quick settings'),
+        SliderPercentagesPreferences.createSwitchRow({
+            title: 'Brightness Slider',
+            subtitle: 'Show a brightness percentage label next to the brightness slider in the quick settings',
+            group: quickSettingsGroup,
+            settings,
+            key: 'quick-settings-brightness'
         });
-
-        quickSettingsGroup.add(quickSettingsBrightnessEnabled);
 
         // OSD group
         const osdGroup = new Adw.PreferencesGroup({
@@ -124,41 +133,36 @@ export default class SliderPercentagesPreferences extends ExtensionPreferences {
         page.add(osdGroup);
 
         // OSD items
-        const osdVolumeEnabled = new Adw.SwitchRow({
-            title: _('Volume Popup'),
-            subtitle: _('Show a volume percentage next to the OSD volume popup'),
+        SliderPercentagesPreferences.createSwitchRow({
+            title: 'Volume Popup',
+            subtitle: 'Show a volume percentage next to the OSD volume popup',
+            group: osdGroup,
+            settings,
+            key: 'osd-volume'
         });
 
-        osdGroup.add(osdVolumeEnabled);
-
-        const osdBrightnessEnabled = new Adw.SwitchRow({
-            title: _('Brightness Popup'),
-            subtitle: _('Show a brightness percentage next to the OSD brightness popup'),
+        SliderPercentagesPreferences.createSwitchRow({
+            title: 'Brightness Popup',
+            subtitle: 'Show a brightness percentage next to the OSD brightness popup',
+            group: osdGroup,
+            settings,
+            key: 'osd-brightness'
         });
 
-        osdGroup.add(osdBrightnessEnabled);
-
-        const osdKeyboardBacklightEnabled = new Adw.SwitchRow({
-            title: _('Keyboard Backlight Popup'),
-            subtitle: _('Show a brightness level next to the OSD keyboard backlight popup (if supported)'),
+        SliderPercentagesPreferences.createSwitchRow({
+            title: 'Keyboard Backlight Popup',
+            subtitle: 'Show a brightness level next to the OSD keyboard backlight popup (if supported)',
+            group: osdGroup,
+            settings,
+            key: 'osd-keyboard-backlight'
         });
-
-        osdGroup.add(osdKeyboardBacklightEnabled);
 
         // Label Group
 
         const labelGroup = SliderPercentagesPreferences.createLabelPrefsGroup(settings);
-
         page.add(labelGroup);
 
         window.add(page);
-
-        settings.bind('quick-settings-volume', quickSettingsVolumeEnabled, 'active', Gio.SettingsBindFlags.DEFAULT);
-        settings.bind('quick-settings-brightness', quickSettingsBrightnessEnabled, 'active', Gio.SettingsBindFlags.DEFAULT);
-
-        settings.bind('osd-volume', osdVolumeEnabled, 'active', Gio.SettingsBindFlags.DEFAULT);
-        settings.bind('osd-brightness', osdBrightnessEnabled, 'active', Gio.SettingsBindFlags.DEFAULT);
-        settings.bind('osd-keyboard-backlight', osdKeyboardBacklightEnabled, 'active', Gio.SettingsBindFlags.DEFAULT);
 
         return Promise.resolve();
     }
